2ac4a6425de3dc0c915be7ef7e92df82d10e0dd1
shriniket <shriniketkulkarni42@gmail.com>
2023-08-09 12:05:54 +0530

    first commit

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.post("/checkout", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: lineItems,
     mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel",
+    success_url: "https://snk-amazon.vercel.app/success",
+    cancel_url: "https://snk-amazon.vercel.app/cancel",
   });
 
   res.send(
